2481f6d11489c75256710076b0a44a369e0d52ca
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-07-29 10:57:11 +0330

    Setup source files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import './styles.css';
+
+console.log("Hello World from the Battleship project.");
+
+// if (process.env.NODE_ENV === 'production')
+//     console.log("We're in production mode!");
+// else
+//     console.log("We're in development mode.");
