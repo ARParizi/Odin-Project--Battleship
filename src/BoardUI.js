8d3a2c6f7a363c049a3acad5ac62918d86df2932
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-08-13 19:27:23 +0330

    Add mouse hover effects

--- src/BoardUI.js
+++ src/BoardUI.js
@@ -78,4 +78,52 @@ export class BoardUI {
         this.renderHits();
         this.renderExclusions();
     }
+
+    placeShipEventListeners(dir, length) {
+        for (let y = 0; y < 10; y++) {
+            for (let x = 0; x < 10; x++) {
+                const square = this.board.querySelector(`.y${y} > .x${x}`);
+                if(this.gameboardObj.placeShipCheck([x, y], dir, length).possible === false)
+                {
+                    square.addEventListener('mouseover', () => this.placeShipMouseHoverEvent(false, x, y, null, null));
+                    square.addEventListener('mouseout',  () => this.placeShipMouseOutEvent  (false, x, y, null, null));
+                } else {
+                    square.addEventListener('mouseover', () => this.placeShipMouseHoverEvent(true, x, y, dir, length));
+                    square.addEventListener('mouseout',  () => this.placeShipMouseOutEvent  (true, x, y, dir, length));
+                }
+            }
+        }
+    }
+
+    placeShipMouseHoverEvent(valid, x, y, dir, length) {
+        if(valid === false) {
+            this.board.querySelector(`.y${y} > .x${x}`).classList.add('ship-place-invalid-hover');
+        }
+        else if(valid) {
+            for (let i = 0; i < length; i++) {
+                if(dir === 'right') {
+                    this.board.querySelector(`.y${y} > .x${x + i}`).classList.add('ship-place-valid-hover');
+                }
+                else if(dir === 'down') {
+                    this.board.querySelector(`.y${y + i} > .x${x}`).classList.add('ship-place-valid-hover');
+                }
+            }
+        }
+    }
+
+    placeShipMouseOutEvent(valid, x, y, dir, length) {
+        if(valid === false) {
+            this.board.querySelector(`.y${y} > .x${x}`).classList.remove('ship-place-invalid-hover');
+        }
+        else if(valid) {
+            for (let i = 0; i < length; i++) {
+                if(dir === 'right') {
+                    this.board.querySelector(`.y${y} > .x${x + i}`).classList.remove('ship-place-valid-hover');
+                }
+                else if(dir === 'down') {
+                    this.board.querySelector(`.y${y + i} > .x${x}`).classList.remove('ship-place-valid-hover');
+                }
+            }
+        }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -11,22 +11,28 @@ import { BoardUI } from './BoardUI.js';
 import { Gameboard } from './Gameboard.js';
 import { Ship } from './Ship.js';
 
-const playerBoardDOM = document.querySelector('#player-board');
+const playerBoardDOM  = document.querySelector('#player-board');
 const playerGameboard = new Gameboard();
+const playerBoardUI   = new BoardUI(playerBoardDOM, playerGameboard);
 
-playerGameboard.placeShip([9, 8], 'down', new Ship(2));
-playerGameboard.placeShip([4, 2], 'down', new Ship(3));
-playerGameboard.placeShip([6, 4], 'right', new Ship(4));
+/*
 playerGameboard.receiveAttack([0, 0]);
 playerGameboard.receiveAttack([0, 2]);
 playerGameboard.receiveAttack([4, 3]);
 playerGameboard.receiveAttack([6, 4]);
 playerGameboard.receiveAttack([9, 9]);
 
-
-const playerBoardUI = new BoardUI(playerBoardDOM, playerGameboard);
 playerBoardUI.renderGrid();
 playerBoardUI.renderShips();
 playerBoardUI.renderMisses();
 playerBoardUI.renderHits();
 playerBoardUI.renderExclusions();
+*/
+
+playerGameboard.placeShip([9, 8], 'down', new Ship(2));
+playerGameboard.placeShip([4, 2], 'down', new Ship(3));
+playerGameboard.placeShip([6, 4], 'right', new Ship(4));
+
+
+playerBoardUI.populatePlayerBoard();
+playerBoardUI.placeShipEventListeners('down', 4);
