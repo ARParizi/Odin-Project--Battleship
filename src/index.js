bb0448788e94fac0f2ce9b8342b841f32a28fb11
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-08-19 10:43:25 +0330

    Deployment config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// import './styles.css';
+import './styles.css';
 
 console.log("Hello World from the Battleship project.");
 
-// if (process.env.NODE_ENV === 'production')
-//     console.log("We're in production mode!");
-// else
-//     console.log("We're in development mode.");
+if (process.env.NODE_ENV === 'production')
+    console.log("We're in production mode!");
+else
+    console.log("We're in development mode.");
 
 import { BoardUI } from './BoardUI.js';
 import { Gameboard } from './Gameboard.js';
